002c81df110d6c555c3756cc24acc3ef035844a0
Suleman Aziz <sulemanaz548@gmail.com>
2025-06-05 14:29:17 -0400

    ..

--- PHP/Custview.php
+++ PHP/Custview.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Customer View</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="css" href="../style.css">
     <script src="Scripts/Functions.js"></script>
 </head>
 
@@ -31,35 +31,35 @@ else {
     <div class="product">
         <label for="product1">
             <input type="radio" name="product" id="product1" value="Cooking Oil" style="display:none;">
-            <img src="product_images\CookingOil.png" alt="Cooking Oil" onclick="selectProduct('product1', this);">
+            <img src="./product_images\CookingOil.png" alt="Cooking Oil" onclick="selectProduct('product1', this);">
         </label>
         <p>Cooking Oil</p>
     </div>
     <div class="product">
         <label for="product2">
             <input type="radio" name="product" id="product2" value="Olpers Cream" style="display:none;">
-            <img src="product_images/OlpersCream.png" alt="Olpers Cream" onclick="selectProduct('product2', this);">
+            <img src="../product_images/OlpersCream.png" alt="Olpers Cream" onclick="selectProduct('product2', this);">
         </label>
         <p>Olpers Cream</p>
     </div>
     <div class="product">
         <label for="product3">
             <input type="radio" name="product" id="product3" value="Chicken Bread" style="display:none;">
-            <img src="product_images\ChickenBread.png" alt="Chicken Bread" onclick="selectProduct('product3', this);">
+            <img src="../product_images\ChickenBread.png" alt="Chicken Bread" onclick="selectProduct('product3', this);">
         </label>
         <p>Chicken Bread</p>
     </div>
     <div class="product">
         <label for="product4">
             <input type="radio" name="product" id="product4" value="Mineral Water" style="display:none;">
-            <img src="product_images\Water.png" alt="Mineral Water" onclick="selectProduct('product4', this);">
+            <img src="../product_images\Water.png" alt="Mineral Water" onclick="selectProduct('product4', this);">
         </label>
         <p>Mineral Water</p>
     </div>
     <div class="product">
         <label for="product5">
             <input type="radio" name="product" id="product5" value="Walls IceCream" style="display:none;">
-            <img src="product_images\IceCream.png" alt="Walls IceCream" onclick="selectProduct('product5', this);">
+            <img src="../product_images\IceCream.png" alt="Walls IceCream" onclick="selectProduct('product5', this);">
         </label>
         <p>Walls IceCream</p>
     </div>
